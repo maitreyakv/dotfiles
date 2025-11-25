7df117a8735235608215c0c1173e57b04ad62c46
Maitreya Venkataswamy <maitreyakv@gmail.com>
2025-11-25 14:01:01 -0500

    feat: adds language servers

--- .config/nvim/lua/config/lazy.lua
+++ .config/nvim/lua/config/lazy.lua
@@ -24,6 +24,17 @@ require("lazy").setup({
         colorscheme = "catppuccin-mocha",
       },
     },
+
+    -- LazyVim extras
+    { import = "lazyvim.plugins.extras.lang.python" },
+    { import = "lazyvim.plugins.extras.lang.rust" },
+    { import = "lazyvim.plugins.extras.lang.docker" },
+    { import = "lazyvim.plugins.extras.lang.json" },
+    { import = "lazyvim.plugins.extras.lang.yaml" },
+    { import = "lazyvim.plugins.extras.lang.typescript" },
+    { import = "lazyvim.plugins.extras.lang.tailwind-css" },
+    { import = "lazyvim.plugins.extras.lang.terraform" }
+
     -- import/override with your plugins
     { import = "plugins" },
   },
