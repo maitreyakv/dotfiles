8e68cff2c6621c0ebece548dfa087bf0aec9aeed
Maitreya Venkataswamy <maitreyakv@gmail.com>
2025-11-25 14:09:57 -0500

    feat: more lang extras

--- .config/nvim/lua/config/lazy.lua
+++ .config/nvim/lua/config/lazy.lua
@@ -32,8 +32,9 @@ require("lazy").setup({
     { import = "lazyvim.plugins.extras.lang.json" },
     { import = "lazyvim.plugins.extras.lang.yaml" },
     { import = "lazyvim.plugins.extras.lang.typescript" },
-    { import = "lazyvim.plugins.extras.lang.tailwind-css" },
-    { import = "lazyvim.plugins.extras.lang.terraform" }
+    { import = "lazyvim.plugins.extras.lang.vue" },
+    { import = "lazyvim.plugins.extras.lang.tailwind" },
+    { import = "lazyvim.plugins.extras.lang.terraform" },
 
     -- import/override with your plugins
     { import = "plugins" },
