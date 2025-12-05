33d43ce475514371465391fbc878265d44b4cfb4
Maitreya Venkataswamy <maitreyakv@gmail.com>
2025-12-05 10:59:40 -0500

    refactor(nix): moves dev tools to nix tools.nix

--- shell.nix
+++ shell.nix
@@ -1,22 +1,11 @@
 let
   nixpkgs = fetchTarball "https://github.com/NixOS/nixpkgs/tarball/nixos-25.11";
   pkgs = import nixpkgs { config = {}; overlays = []; };
+    
+  tools = import ./tools.nix;
 in
 pkgs.mkShellNoCC {
-  packages = with pkgs; [
-    antidote
-    cargo
-    clang
-    curl
-    git
-    neovim
-    nodejs_24
-    starship
-    tmux
-    unzip
-    vivid
-    zsh
-  ];
+  packages = tools ++ [];
 
   shellHook = "zsh";
 }
---
+++ tools.nix
@@ -0,0 +1,21 @@
+# Group of packages that I use for development (e.g. tmux, git, nvim, zsh, etc.)
+let 
+  nixpkgs = fetchTarball "https://github.com/NixOS/nixpkgs/tarball/nixos-25.11";
+  pkgs = import nixpkgs { config = {}; overlays = []; };
+in
+with pkgs; [
+  antidote
+  cargo
+  clang
+  curl
+  git
+  neovim
+  nodejs_24
+  starship
+  tmux
+  unzip
+  vivid
+  zsh
+]
+
+
