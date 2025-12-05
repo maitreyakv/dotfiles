c182ce09f1be9dc154546c62ac90eab68d07e4a1
Maitreya Venkataswamy <maitreyakv@gmail.com>
2025-12-05 11:08:27 -0500

    chore(nix): moves tools.nix to nix folder

--- shell.nix
+++ shell.nix
@@ -2,7 +2,7 @@ let
   nixpkgs = fetchTarball "https://github.com/NixOS/nixpkgs/tarball/nixos-25.11";
   pkgs = import nixpkgs { config = {}; overlays = []; };
     
-  tools = import ./tools.nix;
+  tools = import ./nix/tools.nix;
 in
 pkgs.mkShellNoCC {
   packages = tools ++ [];
