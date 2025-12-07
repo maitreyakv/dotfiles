3fa6977091d6914216e3580847b46b7b76ab0ff9
Maitreya Venkataswamy <maitreyakv@gmail.com>
2025-12-07 18:05:59 -0500

    chore(nixos): cleanups and adding nix-command

--- etc/nixos/configuration.nix
+++ etc/nixos/configuration.nix
@@ -2,7 +2,7 @@
 # your system.  Help is available in the configuration.nix(5) man page
 # and in the NixOS manual (accessible by running ‘nixos-help’).
 
-{ config, pkgs, lib, ... }:
+{ _config, pkgs, lib, ... }:
 
 {
   imports =
@@ -114,10 +114,7 @@
 
   # List packages installed in system profile. To search, run:
   # $ nix search wget
-  environment.systemPackages = with pkgs; [
-  #  vim # Do not forget to add an editor to edit configuration.nix! The Nano editor is also installed by default.
-  #  wget
-  ];
+  environment.systemPackages = [];
 
   # Some programs need SUID wrappers, can be configured further or are
   # started in user sessions.
@@ -146,4 +143,5 @@
   # (e.g. man configuration.nix or on https://nixos.org/nixos/options.html).
   system.stateVersion = "25.11"; # Did you read the comment?
 
+  nix.settings.experimental-features = [ "nix-command" ];
 }
--- nix/tools.nix
+++ nix/tools.nix
@@ -1,9 +1,5 @@
 # Group of packages that I use for development (e.g. tmux, git, nvim, zsh, etc.)
 { pkgs, ... }:
-# let 
-#   nixpkgs = fetchTarball "https://github.com/NixOS/nixpkgs/tarball/nixos-25.11";
-#   pkgs = import nixpkgs { config = {}; overlays = []; };
-# in
 with pkgs; [
   antidote
   cargo
