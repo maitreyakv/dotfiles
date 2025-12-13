654208375740a91c5b35bbd968ddd060871fb0ec
Maitreya Venkataswamy <maitreyakv@gmail.com>
2025-12-13 18:12:19 -0500

    feat(nixos): adds envfs

--- etc/nixos/configuration.nix
+++ etc/nixos/configuration.nix
@@ -52,6 +52,9 @@ in
   # Enable the X11 windowing system.
   services.xserver.enable = true;
 
+  # Enable envfs for binary links
+  services.envfs.enable = true;
+
   # Enable the GNOME Desktop Environment.
   services.displayManager.gdm.enable = true;
   services.desktopManager.gnome.enable = true;
