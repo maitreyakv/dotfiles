d383f10b5929a87af59986afe73a881985ea4b2f
Maitreya Venkataswamy <maitreyakv@gmail.com>
2025-12-13 18:37:16 -0500

    feat(nvim): adds colorcolumn

--- home/.config/nvim/lua/config/options.lua
+++ home/.config/nvim/lua/config/options.lua
@@ -1,6 +1,24 @@
 -- Options are automatically loaded before lazy.nvim startup
--- Default options that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/options.lua
+-- Default options that are always set:
+--   https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/options.lua
 -- Add any additional options here
 
 vim.wo.relativenumber = false
 vim.opt.wrap = true
+
+-- Function to set colorcolumn based on file type
+local function set_colorcolumn()
+  local ft = vim.bo.filetype
+  if ft == "lua" then
+    vim.opt.colorcolumn = "81"
+  elseif ft == "python" then
+    vim.opt.colorcolumn = "89"
+  else
+    vim.opt.colorcolumn = "101"
+  end
+end
+
+-- Create an autocmd for file type changes
+vim.api.nvim_create_autocmd("FileType", {
+  callback = set_colorcolumn,
+})
