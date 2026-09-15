c0745a47b7dd21a62c68620debc9ca88395cac4a
Maitreya Venkataswamy <maitreyakv@gmail.com>
2026-09-15 19:20:18 -0400

    chore: adds todo for pulumi

--- etc/nixos/configuration.nix
+++ etc/nixos/configuration.nix
@@ -204,6 +204,8 @@ in
   # Or disable the firewall altogether.
   # networking.firewall.enable = false;
 
+  # TODO: Figure out how to install latest version of pulumi
+
   # This value determines the NixOS release from which the default
   # settings for stateful data, like file locations and database versions
   # on your system were taken. It‘s perfectly fine and recommended to leave
