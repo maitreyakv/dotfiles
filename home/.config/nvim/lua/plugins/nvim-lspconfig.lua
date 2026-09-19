89505af5344222b9455da9db35d63649bcec4649
Maitreya Venkataswamy <maitreyakv@gmail.com>
2026-09-19 11:38:44 -0400

    fix(nvim): disable lazy loading for nvim-lspconfig

--- home/.config/nvim/lua/plugins/nvim-lspconfig.lua
+++ home/.config/nvim/lua/plugins/nvim-lspconfig.lua
@@ -1,5 +1,8 @@
 return {
   "neovim/nvim-lspconfig",
+  -- `lazy = false` Fixes a bug with filetype detection/LSP loading
+  -- https://github.com/LazyVim/starter/issues/161
+  lazy = false,
   opts = {
     servers = {
       ["tofu-ls"] = {
